7827018718017287f24a1a52d8687d03474b11a9
Kevin Hild <kevin.j.hild@gmail.com>
2024-09-03 12:08:20 +0200

    Ajoute des card

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className='card'>
+            <footer className='card_footer'>
+             <h3 className='title_card'>Titre de la location</h3>   
+            </footer>
+        </div>
+    );
+};
+
+export default Card;
--- src/components/Home/Accueil.jsx
+++ src/components/Home/Accueil.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import Bannier from './Bannier';
+import Card from '../Card';
 
 const Accueil = () => {
     return (
-        <div className='bannier img_bannier'>
+        <div>
+            <div className='bannier img_bannier'>
             <Bannier
                 img="./assets/IMG.jpg"
                 title="Chez vous, partout et ailleurs"
             />
+            </div>
+            <section className='section_card'>
+            <div className='card_contenu'>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            </div>
+            </section>
         </div>
     );
 };
