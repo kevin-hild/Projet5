9313b278e0bd44a8d32a89bdf5fff68784843d03
Kevin Hild <kevin.j.hild@gmail.com>
2024-09-10 10:58:38 +0200

    Modification des page lier au card

--- src/components/Home/LocationDetail.jsx
+++ src/components/Home/LocationDetail.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar, faStar as faStarEmpty } from '@fortawesome/free-solid-svg-icons'; // Utilise la même icône pour les étoiles vides
 import Collapse from '../Collapse';
 
 const LocationDetail = () => {
@@ -20,35 +22,73 @@ const LocationDetail = () => {
         return <div>Chargement...</div>;
     }
 
+    // Fonction pour générer des étoiles avec FontAwesome
+    const renderStars = (rating) => {
+        const fullStars = Math.floor(rating); // Partie entière des étoiles
+        const emptyStars = 5 - fullStars; // Étoiles vides pour compléter
+
+        return (
+            <>
+                {Array.from({ length: fullStars }).map((_, index) => (
+                    <FontAwesomeIcon key={index} icon={faStar} className="star full" />
+                ))}
+                {Array.from({ length: emptyStars }).map((_, index) => (
+                    <FontAwesomeIcon key={index + fullStars} icon={faStar} className="star empty" />
+                ))}
+            </>
+        );
+    };
+
     return (
         <div className='contenu-page'>
-            <div className='img-carouselle'>
+            <div className='img-carousel'>
             <img src={location.cover} alt={location.title} />
             </div>
             <div className='info'>
-                <div>
+                <div className='title-info'>
                     <h1 className='title2'>{location.title}</h1>
                     <p>{location.location}</p>
-                    <p>{location.tags}</p>
+                    <div className="tags-container">
+                        {location.tags.map((tag, index) => (
+                            <span key={index} className="tag">
+                                {tag}
+                            </span>
+                        ))}
+                    </div>
                 </div>
                 <div className="host-info">
-                    <p>{location.host.name}</p>
-                    <img src={location.host.picture} alt={location.host.name} className="host-picture" />
-                    <p>{location.rating}</p>
+                    <div className='name-photo'>
+                    <p>
+                        {location.host.name.split(' ')[0]}<br />
+                        {location.host.name.split(' ')[1]}
+                    </p>
+                        <img src={location.host.picture} alt={location.host.name} className="host-picture" />
+                    </div>
+                    <div className="rating-stars">
+                        {renderStars(parseInt(location.rating, 10))} {/* Remplace la note par des étoiles */}
+                    </div>
                 </div>
             </div>
-            <div>
-            <Collapse
-                    title="Description"
-                    p={location.description}
-                />
+            <div className='description-equipement'>
+                <div className='collapse-description'>
+                    <Collapse
+                        title="Description"
+                        p={location.description}
+                    />
+                </div>
                 <div className='collapse-equipements'>
-                <Collapse
-                    title="Equipments"
-                    p={<ul>{location.equipments}</ul>}
-                /></div>
+                    <Collapse
+                        title="Equipments"
+                        p={(
+                            <ul>
+                              {location.equipments.map((equipment, index) => (
+                                <li key={index}>{equipment}</li>
+                              ))}
+                            </ul>
+                        )}
+                    />
+                </div>
             </div>
-            {/* Ajoutez plus de détails ici comme équipements, tags, etc. */}
         </div>
     );
 };
