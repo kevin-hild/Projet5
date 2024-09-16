5808ea34e4df67481cc43ca03e3dfb9517c6f3c5
Kevin Hild <kevin.j.hild@gmail.com>
2024-09-16 14:25:08 +0200

    Ajout de Media Queries pour la résolution 375px

--- src/components/Erreur404.jsx
+++ src/components/Erreur404.jsx
@@ -5,7 +5,9 @@ const Erreur404 = () => {
     return (
         <div className='page-erreur'>
             <h1>404</h1>
+            <div className='message-erreur'>
             <p>Oups! La page que vous demandez n'existe pas.</p>
+            </div>
             <Link className='lien-retour' to="/">Retour sur la page d'accueil</Link>
         </div>
     );
--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import Logo from "./Logo/LOGO.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+    const location = useLocation(); // Obtenir l'URL actuelle
+
     return (
         <header>
             <img className="logo" src={Logo} alt="Logo Kasa" />
             <nav className="navbar">
                 <div className="nav_link">
-                    <Link to="/">Accueil</Link>
-                    <Link className="aPropos" to="/A-Propos">A Propos</Link>
+                    <Link
+                        to="/"
+                        className={location.pathname === "/" ? "active" : ""}
+                    >
+                        Accueil
+                    </Link>
+                    <Link
+                        to="/A-Propos"
+                        className={location.pathname === "/A-Propos" ? "active" : ""}
+                    >
+                        A Propos
+                    </Link>
                 </div>
             </nav>
         </header>
-    )
+    );
 };
 
 export default Header;
