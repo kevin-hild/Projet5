7802ca80228388e34390f9505194e723af628bba
Kevin Hild <kevin.j.hild@gmail.com>
2024-09-19 09:45:55 +0200

    Modification visuel du titre de la Bannier

--- src/components/Home/Bannier.jsx
+++ src/components/Home/Bannier.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+const Bannier = ({ img, title }) => {
+    const splitTitle = title.split(','); // Sépare le titre en deux parties à partir de la virgule
 
-const Bannier = ({img,title}) => {
     return (
         <div className='bannier'>
             <img className='img_bannier' src={img} alt="" />
-            <h1 className='titre_h1'>{title}</h1>
+            {/* Affiche uniquement le h1 si le titre est non vide */}
+            {title && (
+                <h1 className='titre_h1'>
+                    {splitTitle[0]}
+                    {/* Si le titre a une deuxième partie, on ajoute la virgule et l'espace insécable */}
+                    {splitTitle[1] && `,\u00A0`}
+                    <span className='new-line'>{splitTitle[1]}</span>
+                </h1>
+            )}
         </div>
     );
 };
